b1a593d5cb74cbe6edc37146151bb3e8fe5937a7
Anderson Akoto <49446256+andersonakoto@users.noreply.github.com>
2019-10-01 23:33:56 +0300

    CSC 315 G2 - API ASSIGNMENT
    API developed for testing POST to Database. Use Postman to POST x-www-form-urlencoded keys= firstname, lastname,  patient_age, illness and gender; to URL endpoint http://68.183.232.101/testapi/api.php

---
+++ api.php
@@ -0,0 +1,36 @@
+<?php
+
+$link = mysqli_connect("68.183.232.101", "app-access", "akoto291996", "testapi");
+
+$firstname= $_POST['firstname'];
+
+$lastname = $_POST['lastname'];
+
+$patient_age = $_POST['patient_age'];
+
+$illness = $_POST['illness'];
+
+$gender = $_POST['gender'];
+
+
+if($link->connect_error) {
+
+    exit('Error connecting to database'); 
+  
+  }else{
+
+    $sql = "INSERT INTO patients (patient_id, firstname, lastname, patient_age, illness, gender) VALUES (NULL, '$firstname', '$lastname', '$patient_age', '$illness', '$gender')";
+
+    if (mysqli_query($link, $sql)){
+
+        echo "Submitted Successfully!";
+
+    }else{
+
+        echo "Error in posting! Please try again..." .  mysqli_error($link);
+    }
+
+
+  }
+
+  ?>
